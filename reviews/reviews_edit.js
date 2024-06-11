abdac8f699bc2dd67330cd2b59fe913fce4d8093
Juhyeon Jeong <wwg6710@naver.com>
2024-06-11 17:48:23 +0900

    :sparkles: review 수정 시 이미지 파일도 수정 가능하도록 로직 추가

--- reviews/reviews_edit.js
+++ reviews/reviews_edit.js
@@ -14,45 +14,36 @@ function getReviewDetails() {
         alert('로그인이 필요합니다.');
         return;
     }
-    
 
     axios.get(`http://127.0.0.1:8000/api/reviews/${reviewId}/`, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     })
-        .then(response => {
-            console.log(response)
-            const reviewContent = response.data.review_content;
-            document.getElementById('review-content').value = reviewContent; 
-            const reviewUsername = response.data.username; 
-            console.log(reviewUsername) // juhyeonnn
-            if (checkUserPermission(reviewUsername)) {
-                document.getElementById('review-edit').style.display = 'block'; 
-            } else {
-                alert('이 댓글을 수정할 권한이 없습니다.');
-                document.getElementById('review-content').disabled = true;
-                document.querySelector('#review-edit input[type="submit"]').disabled = true;
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching comment details:', error);
-            alert('댓글 세부 정보를 가져오는 중 오류가 발생했습니다. 콘솔을 확인하세요.');
-        });
+    .then(response => {
+        const reviewContent = response.data.review_content;
+        document.getElementById('review-content').value = reviewContent; 
+        const reviewUsername = response.data.username; 
+        if (checkUserPermission(reviewUsername)) {
+            document.getElementById('review-edit').style.display = 'block'; 
+        } else {
+            alert('이 리뷰를 수정할 권한이 없습니다.');
+            document.getElementById('review-content').disabled = true;
+            document.querySelector('#review-edit input[type="submit"]').disabled = true;
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching review details:', error);
+        alert('리뷰 세부 정보를 가져오는 중 오류가 발생했습니다. 콘솔을 확인하세요.');
+    });
 }
 
 function checkUserPermission(reviewUsername) {
     const loggedInUserId = getUserId(); 
-    console.log(loggedInUserId)
     return loggedInUserId === reviewUsername;
 }
 
-// const loggedInUsername = "juhyeonnn"; 
-// localStorage.setItem('username', loggedInUsername);
-
-
 function getUserId() {
-    console.log(localStorage)
     return localStorage.getItem('username');
 }
 
@@ -60,8 +51,9 @@ document.getElementById('review-edit').addEventListener('submit', function(event
     event.preventDefault(); 
 
     const editedContent = document.getElementById('review-content').value.trim();
+    const imageInput = document.getElementById('image');
 
-    if (editedContent === '') {
+    if (editedContent === '' && !imageInput.files.length) {
         alert('댓글 내용을 입력하세요.');
         return;
     }
@@ -75,20 +67,23 @@ document.getElementById('review-edit').addEventListener('submit', function(event
 
     const formData = new FormData();
     formData.append('review_content', editedContent);
+    if (imageInput.files.length > 0) {
+        formData.append('image', imageInput.files[0]);
+    }
 
     axios.put(`http://127.0.0.1:8000/api/reviews/${reviewId}/update/`, formData, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     })
-        .then(response => {
-            alert('리뷰가 성공적으로 수정되었습니다.');
-            displayReviewList();
-        })
-        .catch(error => {
-            console.error('Error updating comment:', error);
-            alert('리뷰 수정에 실패했습니다. 자세한 내용은 콘솔을 확인하세요.');
-        });
+    .then(response => {
+        alert('리뷰가 성공적으로 수정되었습니다.');
+        displayReviewList();
+    })
+    .catch(error => {
+        console.error('Error updating review:', error);
+        alert('리뷰 수정에 실패했습니다. 자세한 내용은 콘솔을 확인하세요.');
+    });
 });
 
 function displayReviewList() {
